fa3d115f1c759547990ec8f2a17fd67d07b235cc
ArdaCimen <cimenarda.yaz@gmail.com>
2025-01-05 15:38:26 +0300

    Kullanıcı Arayüzü Layout

---
+++ FrontEnds/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_UILayoutNavbarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _UILayoutNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
