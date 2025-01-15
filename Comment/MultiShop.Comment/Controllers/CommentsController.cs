11a6cc3f1e24b3266ec6d3880c0ae75e75a9f9ef
ArdaCimen <cimenarda.yaz@gmail.com>
2025-01-15 13:03:59 +0300

    Comment Microservisi Bütün İşlemleri (Backend,Admin,Uı)

---
+++ Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class CommentsController : ControllerBase
+    {
+
+        private readonly CommentContext _commentContext;
+
+        public CommentsController(CommentContext commentContext)
+        {
+            _commentContext = commentContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _commentContext.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var value = _commentContext.UserComments.Find(id);
+            return Ok(value);
+        }
+        [HttpGet("CommentListByProductID")]
+        public IActionResult CommentListByProductID(string id)
+        {
+            var value = _commentContext.UserComments.Where(x => x.ProductID == id).ToList();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Add(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Başarıyla Eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _commentContext.UserComments.Find(id);
+            _commentContext.UserComments.Remove(value);
+            _commentContext.SaveChanges();
+            return Ok("Başarıyla Silindi");
+
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Update(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Başarıyla Güncellendi");
+        }
+    }
+}
